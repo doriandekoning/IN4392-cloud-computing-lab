e7ec45b94edfbaea8fa0a289bfac36f447c7f91f
jochem725 <jochemlugtenburg@gmail.com>
2018-10-31 19:46:34 +0100

    Fix receiving metrics

--- metriclogger/metriclogger.go
+++ metriclogger/metriclogger.go
@@ -49,7 +49,7 @@ func (measurement Measurement) ToCSVString() string {
 	var metricid = strconv.Itoa(int(measurement.Metric))
 	var value = fmt.Sprintf("%v", measurement.Value)
 
-	return fmt.Sprintf("%s, %s, %s, %s\n", ts, wid, metricid, value)
+	return fmt.Sprintf("%s, %s, %s, %s\n", wid, metricid, value, ts)
 }
 
 func (measurement Measurement) Write(f *os.File) {
